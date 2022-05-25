83a5cfc49a9dc19d804148804c7609b7913c7e22
agsuvidha <agsuvidha@gmail.com>
2022-05-25 16:02:43 +0530

    commit

--- addMarker.js
+++ addMarker.js
@@ -1,60 +1,63 @@
 AFRAME.registerComponent('create-markers',{
     init:async function(){
        var mainScene=document.querySelector('#main-scene')
+
        var dishes=await this.getDishes()
+
        dishes.map(dish=>{
+          alert(JSON.stringify(dish.id))
           
-           var marker=document.createElement('a-marker')
-           marker.setAttribute('id',dish.id)
-           marker.setAttribute('type','pattern')
-           marker.setAttribute('url',dish.marker_pattern)
-           marker.setAttribute('cursor',{
-               rayOrigin:'mouse'
+          var marker=document.createElement("a-marker")
+           marker.setAttribute("id",dish.id)
+           marker.setAttribute("type","pattern")
+           marker.setAttribute("url",dish.marker_pattern_url)
+           marker.setAttribute("cursor",{
+               rayOrigin:"mouse"
            })
-           marker.setAttribute('marker-handler',{})
+           marker.setAttribute("marker-handler",{})
            mainScene.appendChild(marker)
            
-        var model=document.createElement('a-entity')
-        model.setAttribute('id',`model-${dish.id}`)
-        model.setAttribute('position',dish.model_geometry.position)
-        model.setAttribute('rotation',dish.model_geometry.rotation)
-        model.setAttribute('scale',dish.model_geometry.scale)
-        model.setAttribute('gltf-model',`url(${dish.model_url})`)
-        model.setAttribute('gesture-handler',{})
+        var model=document.createElement("a-entity")
+        model.setAttribute("id",`model-${dish.id}`)
+        model.setAttribute("position",dish.model_geometry.position)
+        model.setAttribute("rotation",dish.model_geometry.rotation)
+        model.setAttribute("scale",dish.model_geometry.scale)
+        model.setAttribute("gltf-model",`url(${dish.model_url})`)
+        model.setAttribute("gesture-handler",{})
         marker.appendChild(model)
 
-        var plane=document.createElement('a-plane')
-        plane.setAttribute('id',`plane-${dish.id}`)
-        plane.setAttribute('position',{x:0,y:0,z:0})
-        plane.setAttribute('rotation',{x:-90,y:0,z:0})
-        plane.setAttribute('width',1.7)
-        plane.setAttribute('height',2)
+        var plane=document.createElement("a-plane")
+        plane.setAttribute("id",`plane-${dish.id}`)
+        plane.setAttribute("position",{x:0,y:0,z:0})
+        plane.setAttribute("rotation",{x:-90,y:0,z:0})
+        plane.setAttribute("width",1.7)
+        plane.setAttribute("height",1.5)
         marker.appendChild(plane)
 
-        var tplane=document.createElement('a-plane')
-        tplane.setAttribute('id',`tplane-${dish.id}`)
-        tplane.setAttribute('position',{x:0,y:0.8,z:0})
-        tplane.setAttribute('rotation',{x:-90,y:0,z:0})
-        tplane.setAttribute('width',1.7)
-        tplane.setAttribute('height',0.3)
-        tplane.setAttribute('material',{color:'blue'})
+        var tplane=document.createElement("a-plane")
+        tplane.setAttribute("id",`tplane-${dish.id}`)
+        tplane.setAttribute("position",{x:0,y:0.8,z:0})
+        tplane.setAttribute("rotation",{x:-90,y:0,z:0})
+        tplane.setAttribute("width",1.7)
+        tplane.setAttribute("height",0.3)
+        tplane.setAttribute("material",{color:"blue"})
         plane.appendChild(tplane)
         
-        var disht=document.createElement('a-entity')
-        disht.setAttribute('id',`disht-${dish.id}`)
-        disht.setAttribute('position',{x:0,y:0.8,z:0})
-        disht.setAttribute('rotation',{x:0,y:0,z:0})
-        disht.setAttribute('text',{
-            font:'monoid',color:'yellow',width:1.8,height:1,align:'center',value:dish.dish_name.toUpperCase()
+        var disht=document.createElement("a-entity")
+        disht.setAttribute("id",`disht-${dish.id}`)
+        disht.setAttribute("position",{x:0,y:0,z:0})
+        disht.setAttribute("rotation",{x:0,y:0,z:0})
+        disht.setAttribute("text",{
+            font:"monoid",color:"red",width:2,height:2.5,align:"center",value:dish.dish_name.toUpperCase()
         })
         tplane.appendChild(disht)
 
-        var ingridients=document.createElement('a-entity')
-        ingridients.setAttribute('id',`ingridients-${dish.id}`)
-        ingridients.setAttribute('position',{x:0.5,y:0,z:0.1})
-        ingridients.setAttribute('rotation',{x:0,y:0,z:0})
-        ingridients.setAttribute('text',{
-            font:'monoid',color:'blue',width:2,height:1,align:'left',value:`${dish.ingridients.join('\n\n')}`
+        var ingridients=document.createElement("a-entity")
+        ingridients.setAttribute("id",`ingredients-${dish.id}`)
+        ingridients.setAttribute("position",{x:0.5,y:0,z:0.1})
+        ingridients.setAttribute("rotation",{x:0,y:0,z:0})
+        ingridients.setAttribute("text",{
+            font:"monoid",color:"blue",width:2,height:1,align:"left",value:`${dish.ingredients.join("\n\n")}`
         })
         plane.appendChild(ingridients)
        })
@@ -62,9 +65,11 @@ AFRAME.registerComponent('create-markers',{
     getDishes:async function(){
         return await firebase
         .firestore()
-        .collection('dishes')
+        .collection("dishes")
         .get()
     .then(snap=>{
+        console.log(snap.docs.map(doc => doc.data()))
+
         return snap.docs.map(doc=>doc.data())
     })    }
 
--- markerHandler.js
+++ markerHandler.js
@@ -1,44 +1,63 @@
-AFRAME.registerComponent("marker-handler",{
-    init:async function(){
-        this.el.addEventListener('markerFound',()=>{
-            console.log('marker-found')
-            this.handleMarkerFound()
-        })
+AFRAME.registerComponent("marker-handler", {
+  init: async function () {
 
-        this.el.addEventListener('markerLost',()=>{
-            console.log('marker-lost')
-            this.handleMarkerLost()
-        })
-    },
-    handleMarkerFound:function(){
-        var buttonDiv=document.getElementById('button-div')
-        buttonDiv.style.display='flex'
-       var ratingButton=document.getElementById('rating-button')
-       var orderButton=document.getElementById('order-button')
-      
-       ratingButton.addEventListener('click',function(){
-           swal({
-               icon:'warning',
-               title:'Thanks for ur rating',
-               text:'rating'
-           })
+    var dishes = await this.getDishes();
 
-       })
-       orderButton.addEventListener('click',function(){
-        swal({
-            icon:'warning',
-            title:'Thanks for ur order',
-            text:'order'
-        })
-    })
-
-    },
+    this.el.addEventListener("markerFound", () => {
+      console.log("marker-found");
+      var markerId = this.el.id;
     
-    handleMarkerLost:function(){
-        var buttonDiv=document.getElementById('button-div')
-        buttonDiv.style.display='none'
-        console.log('marker-lost')
-      }
-      
-})
+      this.handleMarkerFound(dishes,markerId);
+    });
 
+    this.el.addEventListener("markerLost", () => {
+      console.log("marker-lost");
+      this.handleMarkerLost();
+    });
+  },
+  handleMarkerFound: function (dishes,markerId) {
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "flex";
+    var ratingButton = document.getElementById("rating-button");
+    var orderButton = document.getElementById("order-button");
+
+    ratingButton.addEventListener("click", function () {
+      swal({
+        icon: "warning",
+        title: "Thanks for ur rating",
+        text: "rating",
+      });
+    });
+    orderButton.addEventListener("click", function () {
+      swal({
+        icon: "warning",
+        title: "Thanks for ur order",
+        text: "order",
+      });
+    });
+
+    var dish = dishes.filter(dish => dish.id === markerId)[0];
+
+    var model = document.querySelector(`#model-${dish.id}`);
+    model.setAttribute("position", dish.model_geometry.position);
+    model.setAttribute("rotation", dish.model_geometry.rotation);
+    model.setAttribute("scale", dish.model_geometry.scale);
+  },
+
+  handleMarkerLost: function () {
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "none";
+    console.log("marker-lost");
+  },
+
+  getDishes: async function () {
+    return await firebase
+      .firestore()
+      .collection("dishes")
+      .get()
+      .then(snap => {
+        return snap.docs.map(doc => doc.data());
+      });
+  },
+
+});
